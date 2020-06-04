c5b3b415e764ebbf9cc7f97a05703cd5b56abf35
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-06-04 16:25:44 -0300

    Test API Express Running

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Running...');
+console.log(' JS Running...');
+
+var espress = require('express');
+var app = espress();
+
+app.get('/', function (req, res) {
+  res.send('API Running...');
+});
+
+app.listen(3000, function () {
+  console.log('API Started!');
+});
