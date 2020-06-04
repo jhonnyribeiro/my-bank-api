73aaf98cf2754c1c9feb8fc334da9b1efc7a5f00
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-06-04 16:54:57 -0300

    metodos post grando arquivo

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 console.log(' JS Running...');
 
-var espress = require('express');
-var app = espress();
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('API Running...');
+});
+
+app.post('/account', (req, res) => {
+  let params = req.body;
+
+  fs.writeFile('accounts.json', JSON.stringify(params), (err) => {
+    console.log(err);
+  });
+
+  res.send('Post Account');
+  console.log('Post Account');
 });
 
 app.listen(3000, function () {
