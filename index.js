cec8eddec150f8739b613d5c55d14ae5011373f5
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-06-05 18:50:22 -0300

    ALTERANDO PARA PROMISES O INDEX.JS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log(' JS Running...');
 
 var express = require('express');
-var fs = require('fs');
+var fs = require('fs').promises;
 var app = express();
 var accountsRouter = require('./routes/accounts.js');
 
@@ -14,9 +14,9 @@ app.get('/', function (req, res) {
   res.send('API Running...');
 });
 
-app.listen(3000, function () {
+app.listen(3000, async () => {
   try {
-    fs.readFile(fileName, 'utf8', (err, data) => {
+    /*fs.readFile(fileName, 'utf8', (err, data) => {
       if (err) {
         const initialJson = {
           nextId: 1,
@@ -30,10 +30,18 @@ app.listen(3000, function () {
         console.log(err);
       }
       console.log(data);
-    });
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
+    });*/
 
-  console.log('API Started!');
+    await fs.readFile(fileName, 'utf8');
+    console.log('API Started!');
+  } catch (error) {
+    const initialJson = {
+      nextId: 1,
+      accounts: [],
+    };
+
+    fs.writeFile(fileName, JSON.stringify(initialJson)).catch((err) => {
+      console.log(err);
+    });
+  }
 });
